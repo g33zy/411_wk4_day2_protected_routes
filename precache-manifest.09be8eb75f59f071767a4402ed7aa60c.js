ff33d5e7b5472a8ed32d3b1898b873a0bc57b6ea
g33zy <stevejgaz@gmail.com>
2023-06-05 19:04:32 -0500

    Updates

---
+++ precache-manifest.09be8eb75f59f071767a4402ed7aa60c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1833766e7777b1cfef7340fcc08e41ac",
+    "url": "/411_wk4_day2_protected_routes/index.html"
+  },
+  {
+    "revision": "1b8604d940bb9bdffb66",
+    "url": "/411_wk4_day2_protected_routes/static/css/main.909db9c4.chunk.css"
+  },
+  {
+    "revision": "85f27ae9887ed3290c03",
+    "url": "/411_wk4_day2_protected_routes/static/js/2.45163695.chunk.js"
+  },
+  {
+    "revision": "1b8604d940bb9bdffb66",
+    "url": "/411_wk4_day2_protected_routes/static/js/main.32d04924.chunk.js"
+  },
+  {
+    "revision": "e1c9e8bec29a5f33d35d",
+    "url": "/411_wk4_day2_protected_routes/static/js/runtime~main.8cfe26ec.js"
+  }
+]);
